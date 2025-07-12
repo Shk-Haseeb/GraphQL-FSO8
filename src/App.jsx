3bab7c85373cb4430cf4e4afa0e03a236dcc91c5
Haseeb <haseeb.shaikh@helsinki.fi>
2025-07-12 17:20:53 +0300

    8.18: Login Functions weel

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
 import { useState } from 'react'
+import { useApolloClient, useQuery } from '@apollo/client'
 import Authors from './components/Authors'
 import Books   from './components/Books'
 import NewBook from './components/NewBook'
+import EditAuthor from './components/EditAuthor'
+import LoginForm  from './components/LoginForm'
+import Notify     from './components/Notify'
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage]       = useState('authors')
+  const [token, setToken]     = useState(null)
+  const [errorMessage, setError] = useState(null)
+  const client = useApolloClient()
+
+  const notify = (msg) => {
+    setError(msg)
+    setTimeout(() => setError(null), 5000)
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.removeItem('library-user-token')
+    client.resetStore()
+    setPage('authors')
+  }
 
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
+
       <nav>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+
+        {token
+          ? <>
+              <button onClick={() => setPage('add')}>add book</button>
+              <button onClick={() => setPage('edit')}>edit author</button>
+              <button onClick={logout}>logout</button>
+            </>
+          : <button onClick={() => setPage('login')}>login</button>
+        }
       </nav>
 
-      <Authors show={page === 'authors'} />
-      <Books   show={page === 'books'} />
-      <NewBook show={page === 'add'} />
+      {page === 'login'     && <LoginForm setToken={setToken} setError={notify}/>}
+      {page === 'authors'   && <Authors show={true}/>}
+      {page === 'books'     && <Books show={true}/>}
+      {page === 'add'       && <NewBook show={true}/>}
+      {page === 'edit'      && <EditAuthor show={true}/>}
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
+// src/main.jsx
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloProvider
+  ApolloProvider,
+  createHttpLink
 } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null
+    }
+  }
+})
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
